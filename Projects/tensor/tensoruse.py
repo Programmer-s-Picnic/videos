a6f78b27713e84c54f9d330df7e032f8b66b11fa
Champak's World <champaksworld@gmail.com>
2026-07-07 15:37:48 +0530

    Update tensoruse.py

--- Projects/tensor/tensoruse.py
+++ Projects/tensor/tensoruse.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 x = np.array([10, 20, 30, 35, 39, 40, 40, 50, 60, 80, 90], dtype=float)
 x = x / 100
 
 y = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], dtype=float)
 
-plt.plot(x,y)
+plt.plot(x, y)
 plt.grid(True)
 plt.xlabel("Input Marks")
 plt.ylabel("Result Pass-1/fail-0")
@@ -29,8 +29,8 @@ model.compile(
 model.fit(x, y, epochs=1000, verbose=0)
 
 test_marks = [17, 35, 40, 75]
-results=[0 for x in test_marks]
-index=0
+results = [0 for x in test_marks]
+index = 0
 for marks in test_marks:
 
     test_value = np.array([marks / 100], dtype=float)
@@ -41,18 +41,18 @@ for marks in test_marks:
 
     if value >= 0.5:
         result = "Pass"
-        results[index]=1
+        results[index] = 1
     else:
         result = "Fail"
-        results[index]=0
-    index+=1
+        results[index] = 0
+    index += 1
     print("Marks:", marks, "Prediction value:",
           round(value, 4), "Result:", result)
-    
-plt.plot(test_marks,y)
+
+plt.plot(test_marks, results)
 plt.grid(True)
 plt.xlabel("Test Marks")
 plt.ylabel("Predicted Result Pass-1/fail-0")
 plt.title("Output Marks vs Predicted Result Pass-1/fail-0")
 plt.legend()
-plt.show()    
+plt.show()
